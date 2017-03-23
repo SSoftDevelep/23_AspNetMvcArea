bb0126ddc8a4b3382dc113df7e0263123787af70
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-23 05:16:17 +0300

    AspNetMvc-Area

---
+++ 23_AspNetMvcArea/Areas/Admin/Controllers/Home1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23_AspNetMvcArea.Areas.Admin.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        // GET: Admin/Home1
+        public ActionResult Index() //Admin Area’dan Customer Area’ya yonlendirme
+        {
+            return RedirectToAction("Index", "Home2", new { area = "Customer" });
+        }
+    }
+}
---
+++ 23_AspNetMvcArea/Areas/SuperUser/Controllers/Home3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _23_AspNetMvcArea.Areas.SuperUser.Controllers
+{
+    public class Home3Controller : Controller
+    {
+        // GET: SuperUser/Home3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
